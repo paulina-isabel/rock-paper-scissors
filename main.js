87b9da43b5d546998e98e26889ddd9d54f8c9873
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-04-23 12:36:09 -0400

    Allow user to choose a fighter, simultaneously assigns computer a random fighter.

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
+//CHOOSE YOUR XYZ MSG selector
 var displayMessage = document.querySelector('#display-message');
 
+//VIEWS selectors
+var homeView = document.querySelector('.home-view');
 var classicGameView = document.querySelector('.classic-game-view');
 var difficultGameView = document.querySelector('.difficult-game-view');
-var homeView = document.querySelector('.home-view');
-// var computerChoiceDisplay = document.querySelector('.computer-choice');
 
+var chosenFighters = document.querySelector('.chosen-fighters');
+
+//BUTTONS selectors
 var changeGameButton = document.querySelector('.change-game-button');
 var classicChoiceButton = document.querySelector('.classic-choice');
 var difficultChoiceButton = document.querySelector('.difficult-choice');
-// var rockButton = document.querySelector('.rock-button');
-// var paperButton = document.querySelector('.paper-button');
-// var scissorsButton = document.querySelector('.scissors-button');
-// var alienButton = document.querySelector('.alien-button');
-// var ufoButton = document.querySelector('.ufo-button');
-// var rocksImage = document.querySelector('.rocks-image');
 
-var computerRock = document.querySelector('.computer-rock');
+//FIGHTERS PIX selector
+var fighters = document.querySelectorAll('.fighter');
 
-classicChoiceButton.addEventListener('click', chooseClassicGame);
-difficultChoiceButton.addEventListener('click', chooseDifficultGame);
-rockButton.addEventListener('click', chooseRock);
-paperButton.addEventListener('click', choosePaper);
-scissorsButton.addEventListener('click', chooseScissors);
-alienButton.addEventListener('click', chooseAlien);
-ufoButton.addEventListener('click', chooseUfo);
+//EVENT LISTENERS
+classicChoiceButton.addEventListener('click', displayClassicGame);
+difficultChoiceButton.addEventListener('click', displayDifficultGame);
+changeGameButton.addEventListener('click', changeGame);
 
+// GLOBE VARS
 var classicGameisChosen = false;
 var difficultGameisChosen = false;
+var humanPlayer = createPlayer('Human', 0);
+var computerPlayer = createPlayer('Computer', 0);
+var classicGame = createGame('Classic');
+var difficultGame = createGame('Difficult');
+var classicVersion = {
+  fighters: ['happy-rock', 'happy-paper', 'happy-scissors']
+};
+var difficultVersion = {
+  fighters: ['happy-rock', 'happy-paper', 'happy-scissors', 'flat-alien', 'ufo']
+};
+var randomClassicIndex = Math.floor(Math.random() * classicVersion.fighters.length);
+var randomClassicFighter = classicVersion.fighters[randomClassicIndex];
+var randomDifficultIndex = Math.floor(Math.random() * difficultVersion.fighters.length);
+var randomDifficultFighter = difficultVersion.fighters[randomDifficultIndex];
 
-var classicGameOptions = ['<img class="computer-choice" src="assets/happy-rocks.png">', '<img class="computer-choice" src="assets/happy-paper.png">', '<img class="computer-choice" src="assets/happy-scissors.png">'];
-var difficultGameOptions = ['<img src="assets/happy-rocks.png">', '<img src="assets/happy-paper.png">', '<img src="assets/happy-scissors.png">', '<img src="assets/flat-alien.png">', '<img src="assets/ufo.png">'];
 
-var computerChoice = [];
-var humanChoice = [];
+// JS *
 
-// *+*+*++*+**+*+*+*+*+*+*+*+*+*+**+*+*+*+*+*+*+**+*+Data Model*+*+**+*+*+*+*+*+**+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*÷
+fighters.forEach((fighter) => {
+  fighter.addEventListener('click', function() {
+    id = this.id;
+    assignFighter();
+  })
+});
+
 function createPlayer(name, wins) {
   var player = {
     playerName: name,
@@ -43,211 +57,68 @@ function createPlayer(name, wins) {
   return player
 }
 
-var humanPlayer = createPlayer('Human', 0);
-var computerPlayer= createPlayer('Computer', 0);
-// console.log(humanPlayer)
-// console.log(computerPlayer)
-
-function createGame(player1, player2, gameType) {
+function createGame(gameType) {
   var game = {
-    player1: player1,
-    player2: player2,
-    gameType: gameType
-    //a way to keep track of data for the game board
-    //a way to keep track of the selected game type
+    player1: humanPlayer,
+    player2: computerPlayer,
+    gameType: gameType,
   }
   return game
 }
 
-var classicVersion = {
-  fighters: ['rock', 'paper', 'scissors']
-};
-var difficultVersion = {
-  fighters: ['rock', 'paper', 'scissors', 'alien', 'ufo']
-};
-
-var classicGame = createGame(humanPlayer, computerPlayer, classicVersion);
-var difficultGame = createGame(humanPlayer, computerPlayer, difficultVersion);
-console.log(classicGame)
-console.log(difficultGame)
-
-//this is so the human & computer obj can be assigned a new key with a random fighter
-var randomClassicIndex = Math.floor(Math.random() * classicVersion.fighters.length);
-var randomClassicFighter = classicVersion.fighters[randomClassicIndex]
-
-randomDifficultIndex = Math.floor(Math.random() * difficultVersion.fighters.length);
-var randomDifficultFighter = difficultVersion.fighters[randomDifficultIndex]
-// console.log(randomDifficultFighter)
-
-function takeClassicTurn(activeClassicPlayer, fighter) {
-  if (activeClassicPlayer === humanPlayer) {
-    activeClassicPlayer.chosenFighter = fighter
-  } else if (activeClassicPlayer === computerPlayer) {
-    activeClassicPlayer.chosenFighter = randomClassicFighter
-    }
-  return activeClassicPlayer
-}
-
-function takeDifficultTurn(activeDifficultPlayer, fighter) {
-  if (activeDifficultPlayer === humanPlayer) {
-    activeDifficultPlayer.chosenFighter = fighter
-  } else if (activeDifficultPlayer === computerPlayer) {
-    activeDifficultPlayer.chosenFighter = randomDifficultFighter
-    }
-  return activeDifficultPlayer
-}
-
-// var firstTurn = takeClassicTurn(computerPlayer)
-// console.log(firstTurn)
-// var secondTurn = takeClassicTurn(humanPlayer, 'scissors')
-// console.log(secondTurn)
-
-var thirdTurn = takeDifficultTurn(computerPlayer)
-// console.log(thirdTurn)
-var fourthTurn = takeDifficultTurn(humanPlayer, 'scissors')
-// console.log(fourthTurn)
-
-// **********RULES*************
-
-//////////classique/////////
-var classicFighters = {
-  rock: {beats: 'scissors'},
-  paper: {beats: 'rock'},
-  scissors: {beats: 'paper'}
-};
-
-// console.log(classicFighters.rock)
-// console.log(classicFighters.rock.beats)
-
-function whoWonClassic(player1, player2) {
-  if (classicFighters[player1.chosenFighter].beats === player2.chosenFighter) {
-    player1.playerWins += 1
-    return `${player1.playerName} won this round!`
-  } else if (classicFighters[player2.chosenFighter].beats === player1.chosenFighter) {
-    player2.playerWins += 1
-    return `${player2.playerName} won this round!`
-  } else {
-    return `It's a draw!`
-  }
-}
-
-// winner = whoWonClassic(humanPlayer, computerPlayer)
-// console.log(classicFighters[humanPlayer.chosenFighter].beats)
-// console.log(computerPlayer.chosenFighter)
-// console.log(humanPlayer.chosenFighter)
-// console.log(computerPlayer.chosenFighter)
-// console.log(humanPlayer.playerWins)
-// console.log(computerPlayer.playerWins)
-// console.log(winner)
-
-//////////difficult/////////
-
-var difficultFighters = {
-  rock: {beats: 'scissors' || 'ufo'},
-  paper: {beats: 'rock' || 'alien'},
-  scissors: {beats: 'paper' || 'ufo'},
-  alien: {beats: 'scissors' || 'rock'},
-  ufo: {beats: 'paper' || 'alien'}
-};
-
-function whoWonDifficult(player1, player2) {
-  if (difficultFighters[player1.chosenFighter].beats === player2.chosenFighter) {
-    console.log('in first')
-    player1.playerWins += 1
-    return `${player1.playerName} won this round!`
-  } else if (difficultFighters[player2.chosenFighter].beats === player1.chosenFighter) {
-    console.log('in second')
-    player2.playerWins += 1
-    return `${player2.playerName} won this round!`
-  } else {
-    console.log('in third')
-    return `It's a draw!`
-  }
-}
-
-winner = whoWonDifficult(humanPlayer, computerPlayer)
-console.log(winner)
-console.log(humanPlayer.playerWins)
-console.log(computerPlayer.playerWins)
-console.log(computerPlayer.chosenFighter)
-console.log(difficultFighters[computerPlayer.chosenFighter].beats)
-
-// *+*+*++*+**+*+*+*+*+*+*+*+*+*+**+*+*+*+*+*+*+**+*+*+*+**+*+*+*+*+*+**+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*÷
-
-// function classicComputerChoice() {
-//   var classicComputerChoice = classicGameOptions[getRandomIndex(classicGameOptions)]
-//   computerChoiceDisplay.innerHTML = classicComputerChoice
-//   computerChoice.push(classicComputerChoice)
-// }
-
-// function difficultComputerChoice() {
-//   var difficultComputerChoice = difficultGameOptions[getRandomIndex(difficultGameOptions)]
-//   computerChoiceDisplay.innerHTML = difficultComputerChoice
-//   computerChoice.push(difficultComputerChoice)
-// }
-
-function chooseClassicGame() {
+function displayClassicGame() {
   homeView.classList.add('hidden');
   classicGameView.classList.remove('hidden');
+  changeGameButton.classList.remove('hidden');
   displayMessage.innerText = "Choose your fighter!";
-  // classicGameisChosen = true;
-}
+  classicGameisChosen = true;
 
-function chooseDifficultGame() {
+};
+
+function displayDifficultGame() {
   difficultGameView.classList.remove('hidden');
   homeView.classList.add('hidden');
+  changeGameButton.classList.remove('hidden');
   displayMessage.innerText = "Choose your fighter!";
-  // difficultGameisChosen = true
+  difficultGameisChosen = true;
+  //the next line creates a game
+  // createGame('difficult')
 }
 
-// function chooseRock() {
-//   paperButton.classList.add('hidden');
-//   scissorsButton.classList.add('hidden');
-//   alienButton.classList.add('hidden');
-//   ufoButton.classList.add('hidden');
-//     if (difficultGameisChosen === true) {
-//       difficultComputerChoice()
-//     } else if (classicGameisChosen === true) {
-//       classicComputerChoice()
-//     }
-// }
+function assignFighter() {
+  humanPlayer.chosenFighter = id;
+  if (classicGameisChosen === true) {
+    computerPlayer.chosenFighter = randomClassicFighter;
+    displayChosenFighter(randomClassicFighter)
+  } else {
+    computerPlayer.chosenFighter = randomDifficultFighter;
+    displayChosenFighter(randomDifficultFighter)
+  }
+  displayChosenFighter(id);   
+};
 
-// function choosePaper() {
-//   rockButton.classList.add('hidden');
-//   scissorsButton.classList.add('hidden');
-//   alienButton.classList.add('hidden');
-//   ufoButton.classList.add('hidden');
-//     if (difficultGameisChosen === true) {
-//       difficultComputerChoice()
-//     } else if (classicGameisChosen === true) {
-//       classicComputerChoice()
-//     }
-// }
+function displayChosenFighter(id) {
+  var chosenFighter = document.getElementById(id);
+  chosenFighters.classList.remove('hidden');
+  classicGameView.classList.add('hidden');
+  difficultGameView.classList.add('hidden');
+  chosenFighters.appendChild(chosenFighter);
+  // chosenFighters.appendChild(randomClassicIndex);
+  // v new code smh v
+  checkDraw()
+  if (classicGame.isDraw === true) {
+    displayMessage.innerText = 'It\'s a draw!'    
+   }
+};
 
-// function chooseScissors() {
-//   rockButton.classList.add('hidden');
-//   paperButton.classList.add('hidden');
-//   alienButton.classList.add('hidden');
-//   ufoButton.classList.add('hidden');
-//     if (difficultGameisChosen === true) {
-//       difficultComputerChoice()
-//     } else if (classicGameisChosen === true) {
-//       classicComputerChoice()
-//     }
-// }
+function checkDraw() {
+  if (humanPlayer.chosenFighter === computerPlayer.chosenFighter) {
+    classicGame.isDraw = true
+  }
+};
 
-// function chooseAlien() {
-//   rockButton.classList.add('hidden');
-//   paperButton.classList.add('hidden');
-//   scissorsButton.classList.add('hidden');
-//   ufoButton.classList.add('hidden');
-//   difficultComputerChoice()
-// }
-
-// function chooseUfo() {
-//   rockButton.classList.add('hidden');
-//   paperButton.classList.add('hidden');
-//   scissorsButton.classList.add('hidden');
-//   alienButton.classList.add('hidden');
-//   difficultComputerChoice()
-// }
+function changeGame() {
+  homeView.classList.remove('hidden');
+  classicGameView.classList.add('hidden');
+  difficultGameView.classList.add('hidden');
+}
